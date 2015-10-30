2fdd8870baea7865166af1fc9cfda035440aa173
AndresOtero <oteroandres95@gmail.com>
2015-10-30 14:01:34 -0300

    Se cargan bien las entidades!!!! Solo queda el error del mover

--- Client/Yaml.cpp
+++ Client/Yaml.cpp
@@ -422,32 +422,34 @@ Juego* Yaml::readServer(){
 					cargarObjetoMapa(pTipos);
 				}
 			} else {
-				LOG_WARNING << "No se define ningun tipo";
-			}
-			/****/
-			printf("\ncarga Yaml\n")
-			/****/
-			if (const YAML::Node *pEscenario = doc.FindValue(tag_escenario)) {
-				escenario= cargarEscenario(conf,pEscenario);
-				if (const YAML::Node *pEntidades = (*pEscenario).FindValue(
-						tag_escenario_entidades)) {
-					for (unsigned i = 0; i < (*pEntidades).size(); i++) {
-						entidades.push_back(cargarEntidad(pEntidades));
+			LOG_WARNING << "No se define ningun tipo";
+		}
+		/****/
+		printf("\ncarga Yaml\n");
+		/****/
+		if (const YAML::Node *pEscenario = doc.FindValue(tag_escenario)) {
+			escenario = cargarEscenario(conf, pEscenario);
+			if (const YAML::Node *pEntidades = (*pEscenario).FindValue(
+					tag_escenario_entidades)) {
+				for (unsigned i = 0; i < (*pEntidades).size(); i++) {
+					Entidad* ent = cargarEntidad(pEntidades);
+					if (ent != NULL) {
+						entidades.push_back(ent);
+						cantidad_de_entidades++;
+
 						/***/
-						Entidad* ent=entidades[i];
-						printf("Nombre: %s\n",ent->objetoMapa->nombre.c_str());
-						printf("X: %d",ent->posicion->getX());
-						printf("Y: %d",ent->posicion->getY());
+						Entidad* ent = entidades[i];
+						printf("Nombre: %s\n", ent->objetoMapa->nombre.c_str());
+						printf("X: %d", ent->posicion->getX());
+						printf("Y: %d", ent->posicion->getY());
 						/**/
 					}
-					escenario->entidades=entidades;
-					printf("\nfin\n")
+				}
+				escenario->entidades = entidades;
 
 				}else {
 					LOG_WARNING << "No hay entidades a instanciar inicialmente";
-					}
-
-
+				}
 			} else {
 				escenario = new Escenario();
 			}
